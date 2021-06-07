a67b17437780a1c5bb015022983b4d73f68fbb3b
Nikita Koshelev <koshelevn92@gmail.com>
2021-06-07 19:51:34 +0300

    test: add index page test

---
+++ test/index.spec.js
@@ -0,0 +1,9 @@
+import { mount } from '@vue/test-utils'
+import indexPage from '@/pages/index.vue'
+
+describe('Index Page', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(indexPage)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
