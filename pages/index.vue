570416b26b17f1cb23164b0a2ac7acc1fda2c412
Nikita Koshelev <koshelevn92@gmail.com>
2021-06-07 19:04:29 +0300

    refactor: remove container

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <div class="heading-page">
+      <h1 class="heading-page__title">Дам почитать, только тебе! 😉</h1>
+      <p class="heading-page__description">
+        Выбери книгу и напиши мне в <a href="#">Телеграм</a>.
+      </p>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {}
-</script>
+<style lang="postcss" scoped>
+.heading-page {
+  margin-top: 140px;
+}
+.heading-page__title {
+  font-size: 2.5rem;
+  line-height: 1.5;
+}
+</style>
