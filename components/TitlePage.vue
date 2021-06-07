3fd998c1e8a84916cab70d29b6ff52152e26ed75
Nikita Koshelev <koshelevn92@gmail.com>
2021-06-07 19:49:54 +0300

    feat: add TitlePage component

---
+++ components/TitlePage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="heading-page">
+      <h1 class="heading-page__title">{{ title }}</h1>
+      <p
+        v-show="subtitle"
+        class="heading-page__description"
+        v-html="subtitle"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitlePage',
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default() {
+        return 'Тут должен быть какой-то загловок! Где-то я ошибся'
+      },
+    },
+    subtitle: {
+      required: false,
+      type: String,
+      default() {
+        return 'Тут должен подзаголвоок, который я не указал'
+      },
+    },
+  },
+}
+</script>
+
+<style lang="postcss" scoped>
+.heading-page {
+  margin-top: 140px;
+}
+.heading-page__title {
+  font-size: 2.5rem;
+  line-height: 1.5;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,19 @@
 <template>
-  <div class="container">
-    <div class="heading-page">
-      <h1 class="heading-page__title">Дам почитать, только тебе! 😉</h1>
-      <p class="heading-page__description">
-        Выбери книгу и напиши мне в <a href="#">Телеграм</a>.
-      </p>
-    </div>
-  </div>
+  <TitlePage :title="title" :subtitle="subtitle" />
 </template>
 
-<style lang="postcss" scoped>
-.heading-page {
-  margin-top: 140px;
+<script>
+export default {
+  data() {
+    return {
+      title: 'Дам почитать, только тебе! 😉',
+      subtitle: 'Выбери книгу и напиши мне в <a href="#">Телеграм</a>',
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+    }
+  },
 }
-.heading-page__title {
-  font-size: 2.5rem;
-  line-height: 1.5;
-}
-</style>
+</script>
