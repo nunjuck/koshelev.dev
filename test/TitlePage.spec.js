9934a7150598f0a02390276e87429703b76ffb5b
Nikita Koshelev <koshelevn92@gmail.com>
2021-06-07 22:58:14 +0300

    test: add props in TitlePage component

--- test/TitlePage.spec.js
+++ test/TitlePage.spec.js
@@ -1,9 +1,10 @@
 import { mount } from '@vue/test-utils'
 import TitlePage from '@/components/TitlePage.vue'
 
-describe('Title Page Component', () => {
+describe('Test Title Page Component', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(TitlePage)
+    const wrapper = mount(TitlePage, { propsData: { title: 'Title page' } })
+
     expect(wrapper.vm).toBeTruthy()
   })
 })
