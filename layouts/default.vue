73f3b202aafb9f4019e9d22a608ba9fe6322ff86
Nikita Koshelev <koshelevn92@gmail.com>
2021-06-13 19:15:28 +0300

    feat: add layout styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <Header />
-    <Nuxt />
+  <div class="layout-wrap">
+    <div class="layout-wrap__header">
+      <Header />
+    </div>
+    <div class="layout-wrap__main">
+      <Nuxt />
+    </div>
+    <div class="layout-wrap__footer">
+      <Footer />
+    </div>
   </div>
 </template>
+
+<style lang="postcss" scoped>
+.layout-wrap {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.layout-wrap__main {
+  flex-grow: 1;
+}
+</style>
